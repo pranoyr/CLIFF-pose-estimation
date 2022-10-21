43dce8265025e0380a46309a78feca2d1eb0c49c
pranoyr <pranoyr@firstsense.tech>
2022-10-21 11:37:30 +0530

    updatedf

--- inference.py
+++ inference.py
@@ -81,7 +81,7 @@ from pose_2D import detect_pose
 
 
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 # print("--------------------------- Detection ---------------------------")
