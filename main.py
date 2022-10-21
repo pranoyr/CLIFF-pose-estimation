efedb9b9c6a8207c62b40af5d783dcadc6603a6e
pranoyr <pranoyr@firstsense.tech>
2022-10-21 13:12:43 +0530

    updatedf

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def main_worker(args):
 	cudnn.benchmark = True
 
 
-	traindir = "/media/pranoy/Pranoy/mpi_inf_3dhp/S1/Seq1/imageFrames/smpl_params"
+	traindir = "/media/pranoy/Pranoy/coco/smpl_params/"
 	# valdir = os.path.join(args.data, 'val')
 	# normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
 	# 								std=[0.229, 0.224, 0.225])
